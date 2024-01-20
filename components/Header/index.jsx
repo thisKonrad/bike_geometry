b2fdba0dd3bbdb60a535d2608d7d4865eaff7181
this.Konrad <konradlebt@gmail.com>
2024-01-20 14:00:31 +0100

    filter method with navBar Link Object added to Header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -3,16 +3,47 @@
 import CollectionSearchBar from '../CollectionSearchBar'
 import Link from 'next/link'
 import styles from '@/styles/Header.module.css'
+import { useLocation } from "react-router-dom";
 
 
 export default function Header ({currentPageTitle}) {
 
+    const location = useLocation();
+
+    const navBarLinks = [
+      { name: "Home", path: "/" },
+      { name: "Info", path: "/info" },
+      { name: "CalculatorErgo", path: "/calculator_ergo" },
+      { name: "Collection", path: "/collection" },
+    ];
+
+    /* ::: show only the routes who are not linking to the current page */
+    const filteredLinks = navBarLinks.filter((link) => link.path !== location.pathname);
+
 return (<>
     <header className={styles.header}>
     <h1>{currentPageTitle}</h1>
-        <nav>
+    <div>
+    <nav>
+        <ul>
+        {filteredLinks.map((link, index) => (
+            <li key={index}>
+             <Link to={link.path}>{link.name}</Link>
+            </li>
+        ))}
+        </ul>
+        </nav>
+    </div>
         <div>
-            <Link href="/">
+            <CollectionSearchBar
+            show={ currentPageTitle === 'Collection'}
+            />
+        </div>
+    </header>
+</>)};
+
+
+          {/*   <Link href="/">
                 <button>Home</button>
             </Link>
             </div>
@@ -31,12 +62,16 @@ return (<>
                 <button>Collection</button>
             </Link>
             </div>
-          </nav>
-          <div>
-            <CollectionSearchBar
-            show={ currentPageTitle === 'Collection'}
-            />
-          </div>
-      </header>
-      </>)
-};
+          </nav> */}
+ 
+
+
+{/* <div className="flex items-center gap-4">
+<ul>
+  {filteredLinks.map((link, index) => (
+    <li key={index}>
+      <Link to={link.path}>{link.name}</Link>
+    </li>
+  ))}
+</ul>
+</div> */}
