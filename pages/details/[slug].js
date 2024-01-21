ef85dfd1fc6a14c496cfd6c26644653e0fe089ce
this.Konrad <konradlebt@gmail.com>
2024-01-21 15:59:17 +0100

    new detail route

--- pages/details/[slug].js
+++ pages/details/[slug].js
@@ -2,7 +2,7 @@
 'use client'
 import Header from '@/components/Header/index.jsx';
 import DetailCard from '@/components/BikeCard';
-//import { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import useSWR from 'swr';
 import styles from '@/styles/Home.module.css';
 
@@ -11,16 +11,23 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function DetailPage() {
 
-    /*    const router = useRouter()
-       const { id } = router.query */
+    const router = useRouter();
+    const { isReady } = router;
+    const { id } = router.query;
+    //const { data, isLoading, error } = useSWR("/api/[id].js", fetcher);
+    const { data, isLoading, error } = useSWR(id ? `/api/${id}` : null);
 
-    const { data, isLoading } = useSWR("/api/[id].js", fetcher);
-    if (isLoading) {
+    if (!isReady || isLoading || error) return <h2 className={styles.loader}>Details Loading...</h2>;
+
+    /* if (isLoading) {`/api/places/${id}`
         return <h1 className={styles.loader}>Details are Loading...</h1>;
-    }
+    } */
     if (!data) {
         console.log('No ID DB Data!');
         return;
+    }
+    if (error) {
+        console.log('error: ', error)
     }
 
     console.log('ID Data from DB: ', data);
