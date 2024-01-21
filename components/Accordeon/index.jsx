4c2b5b8c6a22868d44b472af600ed85cc6c95c8b
this.Konrad <konradlebt@gmail.com>
2024-01-21 11:56:35 +0100

    uninstall accesible accordeon

--- components/Accordeon/index.jsx
+++ components/Accordeon/index.jsx
@@ -1,5 +1,6 @@
 /* :::: Accordeon :::: */
 'use client'
+import Image from 'next/image';
 import {useStore} from '../Zustand';
 //import styles from '@/styles/Home.module.css'
 import InfoStyle from '@/styles/InfoStyle.module.css'
@@ -15,10 +16,23 @@ export default function Accordeon({items}) {
     };
   
    return (
-      <div>
+      <div className={InfoStyle.accordeon_wrap}>
         {items.map((item, index) => (
           <div key={index} className={InfoStyle.accordeon_body}>
-            <header onClick={() => handleClick(index)}>{item.title}</header>
+            <header 
+            onClick={() => handleClick(index)}
+            className={InfoStyle.accordeon_header}
+            >
+            {index === expanded ? 
+            <p className={InfoStyle.accordeon_icon}>
+                &#215;
+            </p> :
+            <p className={InfoStyle.accordeon_icon}>
+                &#43;
+            </p>}
+            {item.title}
+            </header>
+
             <div className={InfoStyle.accordeon_content}>{index === expanded ? item.content : ''}</div>
           </div>
         ))}
--- pages/info/index.jsx
+++ pages/info/index.jsx
@@ -10,14 +10,51 @@ import InfoStyle from '@/styles/InfoStyle.module.css';
 export default function Information() {
 
   const measurementLegend=[
-    {title: 'Framesize',
-    content: 'The size of your frame.'},
-    {title: 'Stack',
-    content: 'The size of your frame.'},
-    {title: 'Reach',
-    content: 'The size of your frame.'},
-  ];
+    {title: 'Inner Leg Measurement or Inside Leg',
+    content: `The inner leg length is measured from crotch to floor, 
+    where your true leg length is measured from the top of your legs to your ankles.
+    You just need this measurement for the calculator here`},
 
+    {title: 'Framesize',
+    content: `Adult bikes are measured by their frame size. 
+    Most manufacturers now measure the frame from the centre of the crank axle, 
+    to the top of the seat tube.`},
+
+    {title: 'Top Tube Length',
+    content: `The length of your top tube is important for the relation between stack an
+    reach and is a important measure to define your seat position.`},
+
+    {title: 'Stack',
+    content: `Stack defines the distance between the bracket ball bearing till
+    the top tube measured from down till up in 90° degrees`},
+
+    {title: 'Reach',
+    content: `Reach is the horizontal distance between stack and the top ending
+    of your steering tube`}, 
+
+    {title: 'Stack to Reach STR',
+    content: `The relation between stack and reach`},
+
+    {title: 'Seattube Angle',
+    content: `The angle of your seattube. This measurement defines your drive handling
+    and your power take off from your legs to the whole bike.
+    When you ride a lower angle you have a bike with a more comfortable 
+    behaviour but less power take off. 
+    Higher angles ending up in a more effective 
+    power take off unit.`},
+
+    {title: 'Saddle Height',
+    content: `The measurement from bottom till the top edge of your saddle`},
+
+    {title: 'Crank Length',
+    content: `The length of your crank related to your inside leg length. 
+    As easy explained:
+    tall pesron ride a longer crank, small persons a shorter crank.
+    Dependend from which ridestyle you prefer you should use 
+    shorter cranks for agile handling and longer cranks 
+    for speed and endurance.`},
+
+  ];
 
 
   return (<>
@@ -28,6 +65,11 @@ export default function Information() {
     <Accordeon
     items={measurementLegend}
     />
+  </main>
+</>)
+}
+
+
 
 {/* <section className={InfoStyle.jumbo}>
       <article className={InfoStyle.article}>
@@ -60,6 +102,3 @@ export default function Information() {
       You can also use a pencil to mark your inside leg on a wall and measure it later.
       </article>
     </section> */}
-  </main>
-</>)
-}
