10adb11cc19f358ac51065abc55e7724fe52363b
this.Konrad <konradlebt@gmail.com>
2024-01-22 12:18:58 +0100

    basic modal style

--- components/DeleteModal/index.jsx
+++ components/DeleteModal/index.jsx
@@ -6,7 +6,31 @@ import DeleteModalStyle from '../../styles/DeleteModalStyle.module.css';
 export default function DeleteModal(){
 
 
-return (<section>
-    
+return (<section className={DeleteModalStyle.modal_wrap}>
+    <div className={DeleteModalStyle.modal_content}>
+        <header className={DeleteModalStyle.header}>
+        <div>
+            <p>Cancel</p>
+            <button
+            className={DeleteModalStyle.close_button}
+            onClick={handleCloseModal}
+            ></button>
+        </div>
+        </header>
+        <article>
+            <p>Are you really sure</p>
+            <p>You want to delete this bike?</p>
+            <p>Changes can not be undone anymore!</p>
+        </article>
+        <footer className={DeleteModalStyle.footer}>
+        <div>
+            <p>Delete</p>
+            <button
+            className={DeleteModalStyle.delete_button}
+            onClick={handleDelete}
+            ></button>
+        </div>
+    </footer>
+    </div>
 </section>)
 }
