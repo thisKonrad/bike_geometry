d9e7b879d9848aed2d0f0841128214572629c458
this.Konrad <konradlebt@gmail.com>
2024-01-23 18:35:47 +0100

    classes for d l mode

--- components/Zustand/index.jsx
+++ components/Zustand/index.jsx
@@ -17,3 +17,5 @@ export const useStore = create((set) => ({
     updateExpanded:(value)=> set({expanded: value}),
    
 }));
+
+   
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,5 +2,6 @@
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
+
   return <Component {...pageProps} />
 }
