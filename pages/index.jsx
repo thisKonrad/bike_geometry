5d511f4871ee8ef0625a175db6c50056232a3434
this.Konrad <konradlebt@gmail.com>
2024-01-29 10:17:26 +0100

    changed article on landing page

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,15 +16,23 @@ export default function Home() {
       <article className={styles.article}>
         <h3>Welcome awesome cyclists!</h3>
         <p>
-        Lets get your fitful personal bicycle measurements.
-        Here you can achieve the basic geometry datas for your new bike project.
-        You need only one measurement.
-        The famous inner leg length also known as inside leg.
-        When you got it, go to the ergonomic calculator and pass your values.
+        <p>Lets start a new bike project.
+        Here you can achieve the basic 
+        geometry datas for a new bike.
+        You only need your inner leg length,
+        also known as inside leg.</p>
+        <br></br>
+        First go to the information page where all 
+        the measurements are well explained to you.
+        Also how to take the inner leg length.
+        When you got it, go to the ergonomic calculator and pass your inner leg length.
         After that you can go to the collection where you can see
-        all bike geometries.
-        Don't know how to measure the inside leg?
-        Go to the info page where all measurements and values are explained for you.</p>
+        a overview of all your bikes.
+        When you click on one of these bike cards you can 
+        see all the values on your detail page.
+        And if you want you can find some related parts for your new bike project
+        @ google when you hit the search buttons beneath the values.
+        </p>
       </article>
     </section>
     </main>
